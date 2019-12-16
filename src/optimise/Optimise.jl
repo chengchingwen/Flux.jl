4db51970c7c77b93b0cd2ca3cf7527b9aeacefe6
chengchingwen <adgjl5645@hotmail.com>
2019-12-17 00:13:56 +0800

    add lookahead optimiser

--- src/optimise/Optimise.jl
+++ src/optimise/Optimise.jl
@@ -2,10 +2,12 @@ module Optimise
 
 export train!,
 	SGD, Descent, ADAM, Momentum, Nesterov, RMSProp,
-	ADAGrad, AdaMax, ADADelta, AMSGrad, NADAM, ADAMW,RADAM, 
-	InvDecay, ExpDecay, WeightDecay, stop, Optimiser
+	ADAGrad, AdaMax, ADADelta, AMSGrad, NADAM, ADAMW,
+  RADAM, Lookahead, InvDecay, ExpDecay, WeightDecay,
+  stop, Optimiser
 
 include("optimisers.jl")
+include("lookahead.jl")
 include("train.jl")
 
 end
