891a82854f718fc4e27e608d1b81cc66240172c7
chengchingwen <adgjl5645@hotmail.com>
2020-07-03 15:06:24 +0800

    Revert to 14956cc
    	remove extra code, back to where only lookahead optimizer
    
    Revert "fix typo"
    
    This reverts commit 342b09c9806eaf9ee30b3f64f47fb6d52dd44e59.
    
    Revert "fieldtypes for julia v1.0"
    
    This reverts commit 994b6672b9214ae344b08493a08f10fb9b809e8f.
    
    revert to 14956c22

--- src/optimise/Optimise.jl
+++ src/optimise/Optimise.jl
@@ -4,11 +4,9 @@ export train!,
   SGD, Descent, ADAM, Momentum, Nesterov, RMSProp,
   ADAGrad, AdaMax, ADADelta, AMSGrad, NADAM, ADAMW,
   RADAM, Lookahead, InvDecay, ExpDecay, WeightDecay,
-  stop, Optimiser, AbstractOptimiser, GradientStyle,
-  StatefulGradient, StatelessGradient
+  stop, Optimiser
 
 include("optimisers.jl")
-include("api.jl")
 include("lookahead.jl")
 include("train.jl")
 
